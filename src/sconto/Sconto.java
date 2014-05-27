21039fccbb6578f1a14bcf0203081252de9d181c
bel22724 <bel22724@LabInf02.belluzzifioravanti.local>
2014-05-27 08:44:05 +0200

    Version 1.0

---
+++ src/sconto/Sconto.java
@@ -0,0 +1,35 @@
+
+package sconto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bel22724
+ */
+public class Sconto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Scanner lettore = new Scanner(System.in);
+        System.out.println("Inserisci il prezzo del prodotto non scontato in euro: ");
+         int prezzo = lettore.nextInt();
+         
+         System.out.println("Inserisci la percentuale di sconto: ");
+         Scanner l = new Scanner(System.in);
+         int percentualesconto = l.nextInt();
+         
+         float sconto = prezzo*percentualesconto/100;
+         
+               
+          float PrezzoScontato = prezzo - sconto;
+         
+         System.out.println("Il prezzo scontato è di: " + PrezzoScontato + " euro ");
+        
+    }
+}
+
